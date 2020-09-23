6722183137643c62a88c2a67fed7d8cac0618b77
stygian-96 <54054289+stygian-96@users.noreply.github.com>
2020-09-23 12:38:06 +0530

    Top Navigation Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './App.css'
+import TopNav from './Components/Navigation/TopNav';
 
 function App() {
   return (
     <div>
-      
+      <TopNav/>
+      {/* <div>SideNav</div>
+      <div>Main Body</div> */}
     </div>
   );
 }
