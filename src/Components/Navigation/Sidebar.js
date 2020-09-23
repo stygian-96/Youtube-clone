a7c2f8d0240a4278f879ddf17636e7a111493a15
stygian-96 <54054289+stygian-96@users.noreply.github.com>
2020-09-24 02:24:16 +0530

    Styled all types of sidebar

--- src/Components/Navigation/Sidebar.js
+++ src/Components/Navigation/Sidebar.js
@@ -1,27 +1,37 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import HomeIcon from '@material-ui/icons/Home';
 import WhatshotIcon from '@material-ui/icons/Whatshot';
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import MenuIcon from '@material-ui/icons/Menu';
 import '../../style/Navigation/sidebar.css'
 
 function Sidebar() {
     return (
-        <div className = "sidebar">
+        <div className = "watch-sidebar-open">
+            <div className = "watch-sidebar-logo show">
+                <div className="watch-sidebar-icon">
+                    <MenuIcon />
+                </div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"
+                    alt="YouTube"
+                    className="watch-sidebar-nav-logo"
+                />
+            </div>
             <div className = "sidebar-item">
                 <HomeIcon className="sidebar-item-icon" />
-                <div>
+                <div className = "sidebar-item-title">
                     Home
                 </div>
             </div>
             <div className = "sidebar-item">
                 <WhatshotIcon className="sidebar-item-icon" />
-                <div>
+                <div className = "sidebar-item-title">
                     Trending
                 </div>
             </div>
             <div className = "sidebar-item">
                 <SubscriptionsIcon className="sidebar-item-icon" />
-                <div>
+                <div className = "sidebar-item-title">
                     Subscriptions
                 </div>
             </div>
