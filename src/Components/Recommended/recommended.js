45448e1d6a94ca9ffef529ac6f3dca9f85b7e479
stygian-96 <54054289+stygian-96@users.noreply.github.com>
2020-09-27 22:17:42 +0530

    Styles home page video cards

--- src/Components/Recommended/recommended.js
+++ src/Components/Recommended/recommended.js
@@ -2,31 +2,53 @@ import React,{useState,useEffect} from 'react'
 import '../../style/Recommended/recommended.css'
 import Axios from 'axios'
 
-const skeletonCards = () => {
-    const row = []
-    for (var i=0; i<28; i++){
-        row.push(
-            <div className="video-card-container">
-                <div className="video-card-thumbnail"></div>
-                <div className="video-card-contents">
-                    <div className = "channel-img"></div>
-                    <div className = "video-details">
-                        <div className="video-details-title"></div>
-                        <div className="video-details-channel-title"></div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-    return row
-}
-
 const Recommended = ({paddingLeft}) => {
     const recommendedBodyPadding = {paddingLeft : `${paddingLeft}`}
     const containerWidth = {width: `calc(100% - 8px)`}
 
     const [loading, setLoading] = useState(true)
     const [videos, setVideos] = useState([])
+
+    const skeletonCards = () => {
+        const row = []
+        for (var i=0; i<28; i++){
+            row.push(
+                <div className="video-card-container">
+                    <div className="video-card-thumbnail"></div>
+                    <div className="video-card-contents">
+                        <div className = "channel-img"></div>
+                        <div className = "video-details">
+                            <div className="video-details-title"></div>
+                            <div className="video-details-channel-title"></div>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+        return row
+    }
+
+    const videoCard = (videos) => {
+        if(videos){
+            return videos.map( video => {
+                return(
+                    <div className="video-card-container" key={video.id}>
+                        <div className="video-card-thumbnail">
+                            <img src={video.snippet.thumbnails.medium.url} alt='Thumbnails' /> 
+                        </div>
+                        <div className="video-card-contents">
+                            <div className = "channel-img"></div>
+                            <div className = "video-details-onsuccess">
+                                <div className="video-details-title-onsuccess">{video.snippet.title}</div>
+                                <div className="video-details-channel-title-onsuccess">{video.snippet.channelTitle}</div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            } )
+        }
+        else return null
+    }
 
     useEffect(() => {
         setLoading(true)
@@ -49,23 +71,9 @@ const Recommended = ({paddingLeft}) => {
         <div className="recommended-body" style={recommendedBodyPadding}>
             <p className="header">Recommended</p>
             <div className="container" style={containerWidth}>
-                {loading ? skeletonCards() : 
-                    videos.map( video => {
-                        return(
-                            <div className="video-card-container" key={video.id}>
-                                <div className="video-card-thumbnail">
-                                    <img src={video.snippet.thumbnails.medium.url} alt='Thumbnails' /> 
-                                </div>
-                                <div className="video-card-contents">
-                                    <div className = "channel-img"></div>
-                                    <div className = "video-details">
-                                        <div className="video-details-title">{video.snippet.title}</div>
-                                        <div className="video-details-channel-title"></div>
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    } )
+                {loading ? 
+                    skeletonCards() : 
+                    videoCard(videos)
                 }
             </div>
         </div>
