4255195cc4db74a82b68c69666244fe773d100b4
stygian-96 <54054289+stygian-96@users.noreply.github.com>
2020-10-02 22:41:51 +0530

    Search Page Improved

--- src/Components/SearchPage/SearchPage.js
+++ src/Components/SearchPage/SearchPage.js
@@ -7,7 +7,7 @@ const SearchPage = (props) =>{
 
     const [searchQuery, setSearchQuery] = useState('')
     const [searchResults, setSearchResults] = useState([])
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(true)
 
     const bodyPadding = {paddingLeft : `${paddingLeft}`}
 
@@ -34,15 +34,32 @@ const SearchPage = (props) =>{
                     q: searchQuery
                 }
             }).then(res => {
-                setLoading(false)
                 setSearchResults([...res.data.items])
             }).catch(err => console.log(err))
         }
     }, [searchQuery])
 
+    useEffect(()=>{
+        if(searchResults.length) setLoading(false)
+    }, [searchResults])
+
     return (
         <div className='search-page-body' style={bodyPadding}>
-            SearchPage {match.params.q}
+            {!loading && 
+            <div className = "search-video-container">
+                <img src = {searchResults[0].snippet.thumbnails.medium.url} alt="No thumbnail" />
+                <div className = "search-video-body">
+                    <p className = "search-video-title" >{searchResults[0].snippet.title}</p>
+                    <p className = "search-video-channel-title" >
+                        {searchResults[0].snippet.channelTitle}
+                        <span>•</span>
+                        Views
+                        <span>•</span>
+                        Time    
+                    </p>
+                    <p className = "search-video-description">{searchResults[0].snippet.description}</p>
+                </div>
+            </div>}
         </div>
     )
 }
---
+++ src/Components/SearchPage/SearchVideoContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchVideoContainer = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
